747b05bcc0af1f8a1cd0f66ca3f9008f3d451833
danctln <catalano.daniella@gmail.com>
2021-10-29 15:33:29 -0300

    primera version

---
+++ js/form.js
@@ -0,0 +1,20 @@
+// Ejemplo de JavaScript para deshabilitar el envío de formularios si hay campos no válidos
+(function () {
+  'use strict'
+
+  // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Bucle sobre ellos y evitar la presentación
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
